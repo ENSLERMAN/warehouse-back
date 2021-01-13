1a23b57b873c4d950518ef570bc61feacc030964
Marsel Garipov <marsel.garipov.rus@gmail.com>
2021-01-13 22:36:04 +0300

    add gomod and models

---
+++ internal/models/users.go
@@ -0,0 +1,11 @@
+package models
+
+type User struct {
+	ID         int64  `json:"id" db:"id"`
+	Surname    string `json:"surname" db:"surname"`
+	Name       string `json:"name db:"name"`
+	Patronymic string `json:"patronymic" db:"patronymic"`
+	Login      string `json:"login" db:"login"`
+	Password   string `json:"password" db:"password"`
+	Access     int64  `json:"access" db:"access"`
+}
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("hello")
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.String(200, "ok")
+	})
+	if err := router.Run(":8080"); err != nil {
+		logrus.Fatalf("Cannot start server with err: %v", err)
+	}
 }

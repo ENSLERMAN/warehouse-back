e02c3d87721de14c174768e1da567939c7b05f34
Marsel Garipov <marsel.garipov.rus@gmail.com>
2021-01-14 00:08:04 +0300

    add new handler get all users

--- internal/service/db.go
+++ internal/service/db.go
@@ -3,11 +3,29 @@ package service
 import (
 	"database/sql"
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func initDb() {
-	_, err := sql.Open("postgres", "")
+	dbUser, exist := os.LookupEnv("DB_LOGIN")
+	if !exist {
+		logrus.Fatalf("DB_LOGIN not exist in .env")
+	}
+	dbPassword, exist := os.LookupEnv("DB_PASSWORD")
+	if !exist {
+		logrus.Fatalf("DB_PASSWORD not exist in .env")
+	}
+	dbURL, exist := os.LookupEnv("DB_URL")
+	if !exist {
+		logrus.Fatalf("DB_URL not exist in .env")
+	}
+	url := "postgres://"+ dbUser + ":" + dbPassword + "@" + dbURL
+	db, err := sql.Open("postgres", url)
 	if err != nil {
 		logrus.Fatalf("cannot connect to database: %v", err)
+	}
+	if err := db.Ping(); err != nil {
+		logrus.Fatalf("cannot ping database: %v", err)
+		return
 	}
 }
